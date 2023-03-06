3464a8cb17c24d0b6b22a2ffeb2f5ada752b972c
matsuda-t <matsuda-t@technonet.co.jp>
2023-03-06 20:53:22 +0900

    ビルド時に動作するように

---
+++ src/polyfill.ts
@@ -0,0 +1,9 @@
+import { Buffer } from "buffer";
+
+if (typeof window.process === "undefined") {
+  (window as any).process = { env: {}, version: "" };
+}
+
+// for csv-parse polyfill
+// https://github.com/vitejs/vite/discussions/2785
+globalThis.Buffer = Buffer;
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,40 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  define: {
-    "window.global": {},
-  },
-  resolve: {
-    alias: {
-      path: "path-browserify",
-      http: "http-browserify",
-      https: "https-browserify",
-      os: "os-browserify/browser",
-      fs: "browserify-fs",
-      crypto: "crypto-browserify",
-      stream: "stream-browserify",
-      process: "process/browser",
+export default defineConfig(({ mode }) => {
+  const envPath = process.cwd();
+  const env = loadEnv(mode, envPath, "");
+  console.log("env.NODE_ENV", env.NODE_ENV);
+  return {
+    define: {
+      "process.env": { DEBUG: env.NODE_ENV === "development" },
+      "window.global": {},
     },
-  },
-  build: {
-    rollupOptions: {
-      external: ["child_process"],
+    resolve: {
+      alias: {
+        path: "path-browserify",
+        http: "http-browserify",
+        https: "https-browserify",
+        os: "os-browserify/browser",
+        fs: "browserify-fs",
+        crypto: "crypto-browserify",
+        stream: "stream-browserify",
+        process: "process/browser",
+        child_process: "process/browser",
+        http2: "http-browserify",
+      },
     },
-  },
-  plugins: [react()],
+    build: {
+      sourcemap: env.NODE_ENV === "development",
+      target: "esnext",
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+      },
+    },
+    plugins: [react()],
+  };
 });
