4140e5c42fee66095858c09aa4f18d478e5c523d
matsuda-t <matsuda-t@technonet.co.jp>
2023-03-07 12:04:41 +0900

    browserifyをrollupプラグインに差し替え

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -4,6 +4,6 @@ if (typeof window.process === "undefined") {
   (window as any).process = { env: {}, version: "" };
 }
 
-// for csv-parse polyfill
-// https://github.com/vitejs/vite/discussions/2785
-globalThis.Buffer = Buffer;
+// // for csv-parse polyfill
+// // https://github.com/vitejs/vite/discussions/2785
+// globalThis.Buffer = Buffer;
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import notifier from "vite-plugin-notifier";
 
 export default defineConfig(({ mode }) => {
   const envPath = process.cwd();
@@ -18,23 +22,33 @@ export default defineConfig(({ mode }) => {
         os: "os-browserify/browser",
         fs: "browserify-fs",
         crypto: "crypto-browserify",
-        stream: "stream-browserify",
-        process: "process/browser",
         child_process: "process/browser",
         http2: "http-browserify",
+        stream: "rollup-plugin-node-polyfills/polyfills/stream",
+        zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
       },
     },
     build: {
       sourcemap: env.NODE_ENV === "development",
       target: "esnext",
+      rollupOptions: {
+        plugins: [rollupNodePolyFill()],
+      },
     },
     optimizeDeps: {
       esbuildOptions: {
         define: {
           global: "globalThis",
         },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true,
+          }),
+          NodeModulesPolyfillPlugin(),
+        ],
       },
     },
-    plugins: [react()],
+    plugins: [react(), notifier()],
   };
 });
